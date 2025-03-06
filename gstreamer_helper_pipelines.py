6536e732150b5f34a66ba964438125c465f495b6
eronariodito <67965868+eronariodito@users.noreply.github.com>
2025-03-06 14:45:48 +0100

    test overlay

--- gstreamer_helper_pipelines.py
+++ gstreamer_helper_pipelines.py
@@ -259,7 +259,7 @@ def DISPLAY_PIPELINE(video_sink='autovideosink', sync='true', show_fps='false', 
         f'{QUEUE(name=f"{name}_videoconvert_q")} ! '
         f'videoconvert name={name}_videoconvert n-threads=2 qos=false ! '
         f'{QUEUE(name=f"{name}_q")} ! '
-        f'fpsdisplaysink name={name} video-sink={video_sink} sync={sync} text-overlay={show_fps} signal-fps-measurements=true font-desc="Sans, 24"'
+        f'fpsdisplaysink name={name} video-sink={video_sink} sync={sync} text-overlay={show_fps} signal-fps-measurements=true !'
     )
 
     return display_pipeline
